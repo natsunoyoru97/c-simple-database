78c9e85b991717f032f6b01de34a34247324b777
natsunoyoru97 <natsunoyoru97@outlook.com>
2022-12-31 18:49:04 +0000

    A: add dependencies - glog, gflags, abseil

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <signal.h>
+#include <sys/signal.h>
+
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+#include "absl/strings/str_join.h"
+
+DEFINE_string(test, "simple database", "the flag for test");
 
 // Tokenizer that takes the space as the delimiter
 std::vector<std::string> Tokenize(const std::string& s) {
@@ -74,8 +82,33 @@ void ExecuteDb(const std::string& command,
   }
 }
 
+// The signal handler for SIGPIPE
+void DoNothing(int signum) { /* Ignore SIGPIPE */ }
+
+void InitGlog(const char* argv0) {
+    google::InitGoogleLogging(argv0);
+}
+
+void InitGflags() {
+    // TODO: Use glog to replace the cout
+    std::cout << FLAGS_test << "\n";
+}
+
+void InitAbseil() {
+    std::vector<std::string> vec { "hello", "world" };
+    std::string s;
+    s = absl::StrJoin(vec, " ");
+
+    // TODO: Use glog to replace the cout
+    std::cout << s << "\n";
+}
+
 int main(int argc, char** argv) {
-  std::cout << "Simple database" << std::endl;
+  InitGlog(argv[0]);
+  InitGflags();
+  InitAbseil();
+
+  signal(SIGPIPE, DoNothing);
 
   while (true) {
     std::string command;
