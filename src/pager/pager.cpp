e2d156da286012a313a67798f5662ef8dcf7f80d
natsunoyoru97 <natsunoyoru97@outlook.com>
2023-01-02 17:43:13 +0000

    Others: temp commit, separate pager into another namespace

---
+++ src/pager/pager.cpp
@@ -0,0 +1,8 @@
+//
+// Created by natsunoyoru on 23-1-2.
+//
+
+#include "pager.h"
+
+namespace pager {
+} // pager
---
+++ src/pager/pager.h
@@ -0,0 +1,16 @@
+//
+// Created by natsunoyoru on 23-1-2.
+//
+
+#ifndef CPP_SIMPLE_DATABASE_PAGER_H
+#define CPP_SIMPLE_DATABASE_PAGER_H
+
+namespace pager {
+
+    class pager {
+
+    };
+
+} // pager
+
+#endif //CPP_SIMPLE_DATABASE_PAGER_H
