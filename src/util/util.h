db207fe902fd2a01e1a9a6bd99174d10b6b93277
natsunoyoru97 <natsunoyoru97@outlook.com>
2023-01-06 16:39:15 +0800

    M: add macros to avoid duplicate reference

--- src/util/util.h
+++ src/util/util.h
@@ -3,9 +3,14 @@
 // Created by natsunoyoru on 23-1-6.
 //
 
+#ifndef SRC_UTIL_UTIL_H_
+#define SRC_UTIL_UTIL_H_
+
 // The size of bytes a page has
 constexpr uint32_t k_page_size = 256;
 // The fixed-size of rows that contains in a page
 constexpr uint32_t rows_per_page = 100;
 // The size of bytes a row has
 constexpr uint32_t row_size = 100;
+
+#endif  // SRC_UTIL_UTIL_H_
