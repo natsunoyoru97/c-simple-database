edbf5240e37f2859bebbd6fecff4f8fa267a317f
natsunoyoru97 <natsunoyoru97@outlook.com>
2023-01-18 00:24:16 +0800

    D: utils/*

--- test/main.cc
+++ test/main.cc
@@ -14,16 +14,26 @@ TEST(DatabaseTest, EchoWorks) { EXPECT_STRNE("hello", "world"); }
 TEST(DatabaseTest, AddWorks) { EXPECT_STREQ("hello", "hello"); }
 
 // Test for Utils
-absl::Status ReturnWithStatus(bool b) {
+absl::Status IsBoolTrue(bool b) {
   return (b) ? absl::OkStatus() : absl::InvalidArgumentError("b is not true");
 }
 
-TEST(UtilsTest, StatusWorks) {
-  EXPECT_EQ(ReturnWithStatus(true).ok(), true);
-  EXPECT_NE(ReturnWithStatus(false).ok(), true);
+absl::StatusOr<std::string> IsStringAlpha(const std::string&& s) {
+  return std::all_of(s.cbegin(), s.cend(), isalpha)
+             ? absl::StatusOr<std::string>(s)
+             : absl::StatusOr<std::string>(absl::InvalidArgumentError(
+                   "the string has non-alpha characters."));
 }
 
-TEST(UtilsTest, StatusOrWorks) {}
+TEST(UtilsTest, StatusWorks) {
+  EXPECT_EQ(IsBoolTrue(true).ok(), true);
+  EXPECT_NE(IsBoolTrue(false).ok(), true);
+}
+
+TEST(UtilsTest, StatusOrWorks) { 
+  EXPECT_EQ(IsStringAlpha("abc").ok(), true); 
+  EXPECT_NE(IsStringAlpha("abc1").ok(), true);
+}
 
 // Tests for query execution
 
