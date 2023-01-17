50fcce1fb15f38d74f8a9613c2ad4f0832d428b5
natsunoyoru97 <natsunoyoru97@outlook.com>
2023-01-17 23:53:36 +0800

    M: use self-built abseil & reformatting

--- test/main.cc
+++ test/main.cc
@@ -1,19 +1,29 @@
 // Copyright 2022 natsunoyoru97
+#include <fcntl.h>
 #include <gtest/gtest.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <cstdio>
 
 #include "absl/status/status.h"
+#include "absl/status/statusor.h"
 
 TEST(DatabaseTest, EchoWorks) { EXPECT_STRNE("hello", "world"); }
 
 TEST(DatabaseTest, AddWorks) { EXPECT_STREQ("hello", "hello"); }
 
 // Test for Utils
-TEST(UtilsTest, StatusWorks) {
-  absl::Status stat = open("basic.db");
-  std::cerr << stat.ok() << "\n";
+absl::Status ReturnWithStatus(bool b) {
+  return (b) ? absl::OkStatus() : absl::InvalidArgumentError("b is not true");
 }
+
+TEST(UtilsTest, StatusWorks) {
+  EXPECT_EQ(ReturnWithStatus(true).ok(), true);
+  EXPECT_NE(ReturnWithStatus(false).ok(), true);
+}
+
+TEST(UtilsTest, StatusOrWorks) {}
 
 // Tests for query execution
 
