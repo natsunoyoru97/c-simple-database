d9e8e360bdf594207c70e73869052c296349c5f0
natsunoyoru <natsunoyoru97@outlook.com>
2023-01-30 02:36:54 +0000

    [Status] Add absl::Status and absl::StatusOr (#11)
    * WIP
    
    * M: fix the code to make it able to compile
    
    * A: util
    
    * D: utils/*
    
    * M: reformatting
    
    * M: modify param type of IsStringAlpha
    
    * M: add Status, stage 1
    
    * M: reformatting
    
    * M: use self-built abseil & reformatting
    
    * D: delete redundant config
    
    * D: basic.db
    
    * M: testcases
    
    ---------
    
    Co-authored-by: dentiny <1910821171@qq.com>

--- test/main.cc
+++ test/main.cc
@@ -1,10 +1,39 @@
 // Copyright 2022 natsunoyoru97
-
+#include <fcntl.h>
 #include <gtest/gtest.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <cstdio>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 
 TEST(DatabaseTest, EchoWorks) { EXPECT_STRNE("hello", "world"); }
 
 TEST(DatabaseTest, AddWorks) { EXPECT_STREQ("hello", "hello"); }
+
+// Test for Utils
+absl::Status IsBoolTrue(bool b) {
+  return (b) ? absl::OkStatus() : absl::InvalidArgumentError("b is not true.");
+}
+
+absl::StatusOr<std::string> IsStringAlpha(std::string s) {
+  return std::all_of(s.cbegin(), s.cend(), isalpha)
+             ? absl::StatusOr<std::string>(s)
+             : absl::StatusOr<std::string>(absl::InvalidArgumentError(
+                   "the string has non-alpha characters."));
+}
+
+TEST(UtilsTest, StatusWorks) {
+  EXPECT_TRUE(IsBoolTrue(true).ok());
+  EXPECT_FALSE(IsBoolTrue(false).ok());
+}
+
+TEST(UtilsTest, StatusOrWorks) {
+  EXPECT_TRUE(IsStringAlpha("abc").ok());
+  EXPECT_FALSE(IsStringAlpha("abc1").ok());
+}
 
 // Tests for query execution
 
